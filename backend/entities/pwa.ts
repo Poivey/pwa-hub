bdfb6ae38e847d9343a907f05047fa4e6855c28c
Antoine Poivey <antoine.poivey@gmail.com>
2018-12-23 12:41:24 +0100

    editorconfig, prettierrc.yaml

---
+++ backend/entities/pwa.ts
@@ -0,0 +1,12 @@
+export interface Pwa {
+  id: string
+  name: string
+  url: string
+  version: string
+  description: string
+  rate: number
+  categories: string[]
+  tags: string[]
+
+  creator_id: string
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,32 @@
-import * as pulumi from "@pulumi/pulumi";
+import { Output } from '@pulumi/pulumi'
+import { Table, API } from '@pulumi/cloud-aws'
+import { Pwa } from './entities/pwa'
+
+const endpoint = new API('pwa-hub-endpoint')
+
+const pwaTable = new Table('pwa', 'id', 'string')
+
+endpoint.get('/api/pwa/{id}', async (req, res) => {
+  const id = req.params['id']
+  try {
+    const pwa: Pwa = await pwaTable.get({ id })
+    if (pwa) {
+      res.status(200).json(pwa)
+      console.log(`GET /url/pwa/${id} => ${pwa}`)
+    } else {
+      res.status(404).json(pwa)
+      console.log(`GET /url/pwa/${id} is missing`)
+    }
+  } catch (err) {
+    res.status(500).json(err.stack)
+    console.log(`GET /url/pwa/${id} error: ${err.stack}`)
+  }
+})
+
+endpoint.post('/api/pwa', async (req, res) => {
+  // TODO read body in a Pwa, return link to new pwa {link : /api/pwa/id, pwa: pwa}
+  // const pwa: Pwa = req.body
+})
+
+export const endpointUrl = endpoint.publish().url
+// read logs : pulumi logs --follow
